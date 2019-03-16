c94cdfbecdda7f857b0f5b6c99fd0c1b1833b118
Dekker <dekker25@gmail.com>
2019-03-16 22:27:59 +0300

    chore(admin): remove unused types

--- admin/src/api/contracts.ts
+++ admin/src/api/contracts.ts
@@ -1,18 +1,3 @@
-import { UserExtended } from '../../../common/contracts/User';
-import { NewsExtended } from '../../../common/contracts/News';
-
-export interface UserListResponse {
-    result: UserExtended[],
-    count: number,
-    receivedAt: number
-}
-
-export interface UserResponse extends UserExtended {
-}
-
-export interface NewsResponse extends NewsExtended {
-}
-
 export interface AuthenticationResponse {
     token: string;
     user: string;
